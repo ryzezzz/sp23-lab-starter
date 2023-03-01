8a2f24af174a8e19f49bd86d46aac2d24188cb1d
wtzz <wentaoryze@163.com>
2023-03-01 16:50:30 +0800

    lab01 complete

--- lab01/ex10_ll_cycle.c
+++ lab01/ex10_ll_cycle.c
@@ -3,4 +3,13 @@
 
 int ll_has_cycle(node *head) {
     /* TODO: Implement ll_has_cycle */
+	if (head == NULL || head->next == NULL) return 0;
+	node *fast = head->next->next, *slow = head ->next;
+	while (fast != slow)
+	{
+		if (!fast->next || !fast->next->next) return 0;
+		fast = fast->next->next;
+		slow = slow->next;
+	}
+	return 1;
 }
